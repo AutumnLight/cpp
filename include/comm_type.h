8b54019ffcada8f6166c6fa6dffe5377928532b7
AutumnLight <zhenxi632965668@gmail.com>
2015-08-11 17:03:30 +0800

    Create comm_type.h
    一些类型的重定义

---
+++ include/comm_type.h
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: commdef.h
+	> Author: zl
+	> Mail: zhaolei123@126.com 
+	> Created Time: Tue 11 Aug 2015 03:46:21 PM CST
+ ************************************************************************/
+
+#include <vector>
+using namespace std;
+
+typedef vector<string> vs;
+typedef vector<int> vint;
+typedef vector<bool> vb;
+typedef vector<vector<string>> vvs;
+typedef vector<vector<int>> vvint;
