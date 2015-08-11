9cc8254a6003fd637ef000368b892963c5884200
lei zhao <zhenxi632965668@gmail.com>
2015-08-11 17:15:30 +0800

    测试文件

---
+++ test.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: test.c
+	> Author: zl
+	> Mail: zhaolei123@126.com 
+	> Created Time: Tue 11 Aug 2015 05:14:26 PM CST
+ ************************************************************************/
+
+#include <stdio.h>
+
+int main() {
+	printf("Hello World!\n");
+	return 0;
+}
